641332396a9be65eb9a10a75ee43699389eaff9b
204003354 <john.patanian@ge.com>
2016-11-02 11:24:15 -0400

    Adding create_template bash script

---
+++ create_template.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Next you need to make the script executable with chmod
+# $ chmod u+x my_shell_script.sh
+touch README.md
+touch requirements.txt
+mkdir data
+mkdir deliver
+mkdir dev
+mkdir figures
+mkdir results
+mkdir src
