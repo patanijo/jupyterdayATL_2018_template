87089a3c757d8036ed8f95b8a2943940ef65747b
204003354 <john.patanian@ge.com>
2018-01-24 10:18:19 -0500

    Adding configuration of git remote'

--- {{ cookiecutter.repo_name }}/hooks/post_gen_project.py
+++ {{ cookiecutter.repo_name }}/hooks/post_gen_project.py
@@ -2,7 +2,13 @@
 
 import subprocess
 
+user_repo = 'https://github.build.ge.com/FleetServicesOfflineAnalytics/' + '{{ cookiecutter.SSO }}/' + {{ repo_name }}'
+
+print('Initializing local git repository')
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '.'])
-subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+subprocess.call(['git', 'commit', '-m', 'Initial Automated commit'])
+
+print('Configuring git remote named origin under your SSO')
+subprocess.call(['git', 'remote', 'add', 'origin', user_repo])
 
